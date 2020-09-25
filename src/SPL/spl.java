52c47eb67e415f4daeef6d1a24cc6490815171bb
grevicoc <cocabc1@gmail>
2020-09-25 23:13:20 +0700

    class spl

--- src/SPL/spl.java
+++ src/SPL/spl.java
@@ -7,7 +7,15 @@ public class spl {
     private int baris,kolom;
     private int[][] SPL = new int[baris][kolom];
 
-    
+    public int getBaris(){
+        return baris;
+    }
+    public int getKolom(){
+        return kolom;
+    }
+    public int[][] setElmt(int row, int column){
+        
+    }
 
 
     
