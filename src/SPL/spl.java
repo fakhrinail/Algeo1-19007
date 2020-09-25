518207c2eefc55565162bd5b0ffaa22bba2cb7cf
fakhrinail <fakhrinailwbw@gmail.com>
2020-09-25 23:07:25 +0700

    Update spl.java
    Input matriks

--- src/SPL/spl.java
+++ src/SPL/spl.java
@@ -1,14 +1,27 @@
 package SPL;
+import java.util.Scanner;
 
 /**
  * spl
  */
 public class spl {
-    private int baris,kolom;
-    private int[][] SPL = new int[baris][kolom];
-
-    
-
-
-    
+    public static void main(String[] args) {
+        try (Scanner scan = new Scanner(System.in)) {
+            System.out.println("Input your row of matrix : ");
+            int row = scan.nextInt();
+            System.out.println("Input your column of matrix : ");
+            int column = scan.nextInt();
+            int[][] matrix = new int[row][column];
+            
+            for (int i = 0; i < row; i++) {
+                for (int j = 0; j < column; j++) {
+                    System.out.println("Masukkan nilai elemen baris ke-" + (i+1) + " kolom ke-" + (j+1) +": ");
+                    matrix[i][j] = scan.nextInt();
+                }
+            }
+            
+        } catch (Exception e) {
+            System.out.println("Invalid input");
+        }
+    }
 }
