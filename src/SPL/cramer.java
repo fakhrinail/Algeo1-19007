e536169757c17ffb5fad5dfbd6b577c60971a4fa
fakhrinail <fakhrinailwbw@gmail.com>
2020-09-28 22:15:43 +0700

    Update cramer.java
    Problem NullPointerException

--- src/SPL/cramer.java
+++ src/SPL/cramer.java
@@ -1,5 +1,7 @@
 package SPL;
 
+import java.util.Scanner;
+
 public class cramer {
     private int baris,kolom;
     private float[][] tabFloat;
@@ -58,14 +60,19 @@ public class cramer {
     }
 
     /*METHOD*/
+    public void copyMatriks(cramer MSource){
+        this.baris = MSource.baris;
+        this.kolom = MSource.kolom;
+        this.tabFloat = MSource.tabFloat;
+    }
 
     public float[][] switchColumn(float[][] matriks, int columntarget) {
         float tmp;
 
         for (int i = 0; i < matriks.length; i++) {
             tmp = matriks[i][columntarget];
-            matriks[i][columntarget] = matriks[i][column];
-            matriks[i][column] = tmp;
+            matriks[i][columntarget] = matriks[i][matriks[0].length-1];
+            matriks[i][matriks[0].length-1] = tmp;
         }
 
         return matriks;
@@ -76,7 +83,7 @@ public class cramer {
         square = new float[matriks.length][matriks[0].length-1];
 
         for (int i = 0; i < matriks.length; i++) {
-            for (int j = 0; j < matriks.length; j++) {
+            for (int j = 0; j < matriks[0].length-1; j++) {
                 square[i][j] = matriks[i][j];
             }
         }
@@ -88,39 +95,50 @@ public class cramer {
         if (matriks.length==2 && matriks[0].length==2){
             return (matriks[0][0]*matriks[1][1]) - (matriks[0][1]*matriks[1][0]);
         }else{
-            
+            int i = 0;
+            int j;
+
             float determinan=0;
             for (j=0;j<matriks[0].length;j++){  
                 float[][] mtmp;
                 mtmp = new float[matriks.length-1][matriks[0].length-1];
         
-                indeks k,l;
+                int k,l;
                 for (k=1;k<matriks.length;k++){
-                for (l=0;l<matriks[0].length;l++){
-                    if (l<j){
-                        mtmp[k-1][l] = matriks[k][l];
-                    }else if (l>j){
-                        mtmp[k-1][l-1] = mtmp[k][l];
-                    }
+                    for (l=0;l<matriks[0].length;l++){
+                        if (l<j){
+                            mtmp[k-1][l] = matriks[k][l];
+                        }else if (l>j){
+                            mtmp[k-1][l-1] = matriks[k][l];
+                        }
                 }
+                }
+                float isneg = (j%2==0) ? 1:-1;
+                determinan += isneg * matriks[i][j] * determinan(mtmp);
             }
-            float isneg = (j%2==0) ? 1:-1;
-            determinan += isneg * matriks[i][j] * Determinan(mtmp);
-            }
-                return determinan;
-            }
+            return determinan;
+        }
     }
     
     public float[] cramerMethod(float[][] matriks){
-        float[] hasil;
-        hasil = new float[matriks.length];
+        float[] hasil = new float[matriks.length];
         float pembagi;
 
         pembagi = determinan(squareMatrix(matriks));
         for (int i = 0; i < hasil.length; i++) {
-            hasil[i] = determinan(switchColumn(matriks, i)) / pembagi;
+            hasil[i] = determinan(squareMatrix(switchColumn(matriks, i))) / pembagi;
         }
 
         return hasil;
+    }
+
+    public void printHasil(){
+        float[] hasil = cramerMethod(tabFloat);
+        for (Integer i = 0; i < hasil.length; i++) {
+            System.out.print("Hasil elemen ke-");
+            System.out.print(i+1);
+            System.out.print(" adalah ");
+            System.out.print(hasil[i]);
+        }
     }
 }
